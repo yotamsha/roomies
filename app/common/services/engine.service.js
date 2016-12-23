64bce03ed86d1ee79b7f2f0aa485889cc0dbdedd
yotam shalev <yotamsha@gmail.com>
2016-12-23 19:39:58 +0200

    Added Inventory and navigation between the sections

--- app/common/services/engine.service.js
+++ app/common/services/engine.service.js
@@ -6,38 +6,243 @@ angular.module('myApp.services')
     .service('EngineService', ['$rootScope', function ($rootScope) {
         var _stage, _renderer, _stageItems = [];
         var _images = [
-            {
-                path: 'armchair.png'
-            },
-            {
-                path: 'plant-2.png'
-            },
-            {
-                path: 'plant-3.png'
-            },
-            {
-                path: 'bed.png',
-                width: 300,
-                height: 300,
-            },
-            {
-                path: 'closet.png',
-                width: 200,
-                height: 200,
-            },
-            {
-                path: 'chest-of-drawers-2.png',
-                width: 200,
-                height: 200,
-            },
-        ];
+            {"path": "air-conditioner-1.png"},
+            {"path": "armchair.png"},
+            {"path": "armchair-1.png"},
+            {"path": "armchair-2.png"},
+            {"path": "armchair-3.png"},
+            {"path": "armchair-4.png"},
+            {"path": "armchair-5.png"},
+            {"path": "armchair-6.png"},
+            {"path": "armchair-7.png"},
+            {"path": "bathtub.png"},
+            {"path": "bathtub-1.png"},
+            {"path": "bathtub-2.png"},
+            {"path": "bed.png"},
+            {"path": "bed-1.png"},
+            {"path": "bed-2.png"},
+            {"path": "blender.png"},
+            {"path": "boiler.png"},
+            {"path": "bonsai.png"},
+            {"path": "bookcase.png"},
+            {"path": "bookcase-1.png"},
+            {"path": "bookshelf.png"},
+            {"path": "bookshelf-1.png"},
+            {"path": "bookshelf-2.png"},
+            {"path": "bookshelf-3.png"},
+            {"path": "bookshelf-4.png"},
+            {"path": "cactus.png"},
+            {"path": "cactus-1.png"},
+            {"path": "chair.png"},
+            {"path": "chair-1.png"},
+            {"path": "chair-2.png"},
+            {"path": "chair-3.png"},
+            {"path": "chair-4.png"},
+            {"path": "chair-5.png"},
+            {"path": "chandelier.png"},
+            {"path": "chest-of-drawers.png"},
+            {"path": "chest-of-drawers-1.png"},
+            {"path": "chest-of-drawers-2.png"},
+            {"path": "chest-of-drawers-3.png"},
+            {"path": "chest-of-drawers-4.png"},
+            {"path": "chest-of-drawers-5.png"},
+            {"path": "chest-of-drawers-6.png"},
+            {"path": "chest-of-drawers-7.png"},
+            {"path": "closet.png"},
+            {"path": "closet-1.png"},
+            {"path": "closet-2.png"},
+            {"path": "closet-3.png"},
+            {"path": "closet-4.png"},
+            {"path": "coffee-machine.png"},
+            {"path": "cooker.png"},
+            {"path": "cooler.png"},
+            {"path": "couch.png"},
+            {"path": "couch-1.png"},
+            {"path": "couch-2.png"},
+            {"path": "couch-3.png"},
+            {"path": "couch-4.png"},
+            {"path": "couch-5.png"},
+            {"path": "couch-6.png"},
+            {"path": "couch-7.png"},
+            {"path": "couch-8.png"},
+            {"path": "couch-9.png"},
+            {"path": "couch-10.png"},
+            {"path": "couch-11.png"},
+            {"path": "desk.png"},
+            {"path": "desk-1.png"},
+            {"path": "desk-2.png"},
+            {"path": "desk-3.png"},
+            {"path": "desk-4.png"},
+            {"path": "dishwasher.png"},
+            {"path": "door.png"},
+            {"path": "door-1.png"},
+            {"path": "door-2.png"},
+            {"path": "door-3.png"},
+            {"path": "door-4.png"},
+            {"path": "door-5.png"},
+            {"path": "door-6.png"},
+            {"path": "dressing.png"},
+            {"path": "dressing-1.png"},
+            {"path": "dressing-2.png"},
+            {"path": "fireplace.png"},
+            {"path": "fireplace-1.png"},
+            {"path": "fireplace-2.png"},
+            {"path": "fireplace-3.png"},
+            {"path": "flower.png"},
+            {"path": "flower-1.png"},
+            {"path": "food-steamer.png"},
+            {"path": "fridge.png"},
+            {"path": "fridge-1.png"},
+            {"path": "fridge-2.png"},
+            {"path": "garage.png"},
+            {"path": "hanger.png"},
+            {"path": "hanger-1.png"},
+            {"path": "hanger-2.png"},
+            {"path": "heater.png"},
+            {"path": "hood.png"},
+            {"path": "iron.png"},
+            {"path": "juicer.png"},
+            {"path": "lamp.png"},
+            {"path": "lamp-1.png"},
+            {"path": "lamp-2.png"},
+            {"path": "lamp-3.png"},
+            {"path": "lamp-4.png"},
+            {"path": "lamp-5.png"},
+            {"path": "lamp-6.png"},
+            {"path": "lamp-7.png"},
+            {"path": "library.png"},
+            {"path": "light-bulb.png"},
+            {"path": "light-bulb-1.png"},
+            {"path": "light-bulb-2.png"},
+            {"path": "light-bulb-3.png"},
+            {"path": "light-bulb-4.png"},
+            {"path": "light-bulb-5.png"},
+            {"path": "light-bulb-6.png"},
+            {"path": "light-bulb-7.png"},
+            {"path": "light-bulb-8.png"},
+            {"path": "light-bulb-9.png"},
+            {"path": "lights.png"},
+            {"path": "living-room.png"},
+            {"path": "livingroom.png"},
+            {"path": "livingroom-1.png"},
+            {"path": "livingroom-2.png"},
+            {"path": "meat-grinder.png"},
+            {"path": "microwave.png"},
+            {"path": "microwave-1.png"},
+            {"path": "mixer.png"},
+            {"path": "mixer-1.png"},
+            {"path": "mixer-2.png"},
+            {"path": "mixer-3.png"},
+            {"path": "nightstand.png"},
+            {"path": "nightstand-1.png"},
+            {"path": "nightstand-2.png"},
+            {"path": "office-chair.png"},
+            {"path": "office-chair-1.png"},
+            {"path": "office-chair-2.png"},
+            {"path": "office-chair-3.png"},
+            {"path": "office-chair-4.png"},
+            {"path": "office-chair-5.png"},
+            {"path": "office-chair-6.png"},
+            {"path": "office-chair-7.png"},
+            {"path": "office-chair-8.png"},
+            {"path": "office-chair-9.png"},
+            {"path": "office-chair-10.png"},
+            {"path": "office-chair-11.png"},
+            {"path": "ottoman.png"},
+            {"path": "pipe.png"},
+            {"path": "pipe-1.png"},
+            {"path": "pipe-2.png"},
+            {"path": "pipe-3.png"},
+            {"path": "pipe-4.png"},
+            {"path": "pipe-5.png"},
+            {"path": "pipe-6.png"},
+            {"path": "plant.png"},
+            {"path": "plant-1.png"},
+            {"path": "plant-2.png"},
+            {"path": "plant-3.png"},
+            {"path": "radiator.png"},
+            {"path": "radiator-1.png"},
+            {"path": "radiator-2.png"},
+            {"path": "radiator-3.png"},
+            {"path": "radiator-4.png"},
+            {"path": "sandwich-maker.png"},
+            {"path": "sewing-machine.png"},
+            {"path": "shower.png"},
+            {"path": "shower-1.png"},
+            {"path": "shower-2.png"},
+            {"path": "sink.png"},
+            {"path": "sink-1.png"},
+            {"path": "sofa.png"},
+            {"path": "sofa-1.png"},
+            {"path": "sofa-2.png"},
+            {"path": "sofa-3.png"},
+            {"path": "sofa-4.png"},
+            {"path": "sofa-5.png"},
+            {"path": "sofa-6.png"},
+            {"path": "sofa-7.png"},
+            {"path": "sofa-8.png"},
+            {"path": "sofa-9.png"},
+            {"path": "sofa-10.png"},
+            {"path": "sofa-11.png"},
+            {"path": "sofa-12.png"},
+            {"path": "sofa-13.png"},
+            {"path": "sofa-14.png"},
+            {"path": "sofa-15.png"},
+            {"path": "sofa-16.png"},
+            {"path": "sofa-17.png"},
+            {"path": "sofa-18.png"},
+            {"path": "stairs.png"},
+            {"path": "stairs-1.png"},
+            {"path": "stool.png"},
+            {"path": "stool-1.png"},
+            {"path": "stool-2.png"},
+            {"path": "stove.png"},
+            {"path": "stove-1.png"},
+            {"path": "table.png"},
+            {"path": "table-1.png"},
+            {"path": "table-2.png"},
+            {"path": "table-3.png"},
+            {"path": "table-4.png"},
+            {"path": "table-5.png"},
+            {"path": "table-6.png"},
+            {"path": "table-7.png"},
+            {"path": "tap.png"},
+            {"path": "television.png"},
+            {"path": "television-1.png"},
+            {"path": "television-2.png"},
+            {"path": "television-3.png"},
+            {"path": "television-4.png"},
+            {"path": "toilet.png"},
+            {"path": "tulips.png"},
+            {"path": "vacuum-cleaner.png"},
+            {"path": "valve.png"},
+            {"path": "valve-1.png"},
+            {"path": "valve-2.png"},
+            {"path": "wall.png"},
+            {"path": "wardrobe.png"},
+            {"path": "wardrobe-1.png"},
+            {"path": "wardrobe-2.png"},
+            {"path": "washing-machine.png"},
+            {"path": "washing-machine-1.png"},
+            {"path": "water-heater.png"},
+            {"path": "window.png"},
+            {"path": "window-1.png"},
+            {"path": "window-2.png"},
+            {"path": "window-3.png"},
+            {"path": "window-4.png"},
+            {"path": "window-5.png"},
+            {"path": "window-6.png"},
+            {"path": "window-7.png"},
+            {"path": "window-8.png"},
+            {"path": "window-9.png"}]
+
         var Consts = {
             FLOOR_Y_THRESHOLD: 513,
             DEFAULT_SCALE: 0.2,
-            ITEM_WIDTH : 100,
-            ITEM_HEIGHT : 100,
-            STAGE_WIDTH : 1300,
-            STAGE_HEIGHT : 700,
+            ITEM_WIDTH: 100,
+            ITEM_HEIGHT: 100,
+            STAGE_WIDTH: 1300,
+            STAGE_HEIGHT: 700,
         }
         var basePath = "../../assets/images/furniture/";
         // Drag is the best layer, dragged element is above everything else
@@ -66,9 +271,9 @@ angular.module('myApp.services')
             stageItem.anchor.set(0.5);
             stageItem.width = width;
             stageItem.height = height;
-/*
-            stageItem.scale.set(scale)
-*/
+            /*
+             stageItem.scale.set(scale)
+             */
             // setup events
             stageItem
             // events for drag start
@@ -91,12 +296,12 @@ angular.module('myApp.services')
             // add it to the stage
             _stage.addChild(stageItem);
             _stageItems.push({
-                key : stageItem.key,
-                x : stageItem.position.x,
-                y : stageItem.position.y,
-                width : stageItem.width,
-                height : stageItem.height,
-                image : stageItem.imageKey,
+                key: stageItem.key,
+                x: stageItem.position.x,
+                y: stageItem.position.y,
+                width: stageItem.width,
+                height: stageItem.height,
+                image: stageItem.imageKey,
             });
         }
 
@@ -116,13 +321,14 @@ angular.module('myApp.services')
             gr.beginFill(0x0, 1);
             //yes , I know bunny size, I'm sorry for this hack
             var scale = 1.1;
-            gr.drawRect(obj.width/2, obj.width/2, obj.width, obj.width);
+            gr.drawRect(obj.width / 2, obj.width / 2, obj.width, obj.width);
             gr.endFill();
             gr.filters = [blurFilter];
 
             // gr.displayGroup = shadowLayer;
             obj.addChild(gr);
         }
+
         /*        function onthisDropped(event){
          var resizing = false;
          if (resizing){
@@ -160,10 +366,11 @@ angular.module('myApp.services')
 
          }*/
 
-        function isInFloorArea(object){
+        function isInFloorArea(object) {
             var OFFSET = 35; // y offset is needed to make sure the item bottom is tested against the threshold.
             return (object.position.y + (object.height / 2) - OFFSET > Consts.FLOOR_Y_THRESHOLD);
         }
+
         function onDragStart(event) {
             // store a reference to the data
             // the reason for this is because of multitouch
@@ -186,7 +393,7 @@ angular.module('myApp.services')
             var distance = this.position.y - Consts.FLOOR_Y_THRESHOLD;
             if (isInFloorArea(this)) { // the object was dragged towards the floor
                 console.log("In floor!!!!!");
-                var scale = Math.ceil((distance + 100) / 100) ;
+                var scale = Math.ceil((distance + 100) / 100);
                 this.width = this.metaData.originalWidth * (1 + scale / 10);
                 this.height = this.metaData.originalHeight * (1 + scale / 10);
                 this.displayGroup = floorLayer;
@@ -197,22 +404,22 @@ angular.module('myApp.services')
                 this.displayGroup = defaultLayer;
 
             }
-            $rootScope.$broadcast('STAGE_OBJECT_CHANGED',{
+            $rootScope.$broadcast('STAGE_OBJECT_CHANGED', {
                 key: this.key,
                 height: this.height,
                 width: this.width,
                 x: this.position.x,
                 y: this.position.y,
-                image : this.imageKey
+                image: this.imageKey
             });
         }
 
         function onDragMove() {
             if (this.dragging) {
-                if (_lastDraggedObject && _lastDraggedObject !== this ){ // remove last dragged object from top.
+                if (_lastDraggedObject && _lastDraggedObject !== this) { // remove last dragged object from top.
                     _lastDraggedObject.displayGroup = this.oldGroup;
                 }
-                if(!isInFloorArea){
+                if (!isInFloorArea) {
                     this.displayGroup = defaultLayer;
                 } else {
                     this.displayGroup = floorLayer;
@@ -234,7 +441,7 @@ angular.module('myApp.services')
                 //specify display list component
                 _stage.displayList = new PIXI.DisplayList();
 
-                if (roomData){
+                if (roomData) {
                     for (var key in roomData.items) {
 
                         var item = roomData.items[key];
@@ -247,7 +454,7 @@ angular.module('myApp.services')
                     requestAnimationFrame(animate);
                 } else {
                     // create a texture from an image path
-                    var len = _images.length;
+/*                    var len = _images.length;
                     for (var i = 0; i < len; i++) {
                         var image = _images.pop()
                         var width = image.width || Consts.ITEM_WIDTH;
@@ -256,12 +463,10 @@ angular.module('myApp.services')
                             Math.floor(Math.random() * Consts.STAGE_WIDTH), Math.floor(Math.random() * Consts.STAGE_HEIGHT));
                     }
                     requestAnimationFrame(animate);
-                    $rootScope.$broadcast('STAGE_CREATED',{
-                        items : _stageItems
-                    });
+                    $rootScope.$broadcast('STAGE_CREATED', {
+                        items: _stageItems
+                    });*/
                 }
-
-
 
 
             }
--- app/views/room/room.js
+++ app/views/room/room.js
@@ -2,89 +2,95 @@
 
 angular.module('myApp.room', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/room', {
-    templateUrl: 'views/room/room.html',
-    controller: 'RoomCtrl',
-    controllerAs: 'ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/room', {
+            templateUrl: 'views/room/room.html',
+            controller: 'RoomCtrl',
+            controllerAs: 'ctrl'
+        });
+    }])
 
-.controller('RoomCtrl', ['$scope','EngineService','$firebaseObject','$rootScope','RoomService',
-  function($scope, EngineService, $firebaseObject, $rootScope, RoomService) {
-  var ctrl = this;
-  var Rooms = firebase.database().ref('roomies/rooms');
-  
-  function _init(){
-    ctrl.roomId = window.localStorage.roomId;
-    if (window.localStorage && window.localStorage.roomId){
-      RoomService.getRoom(window.localStorage.roomId).then(function(data){
-        $scope.roomData = data;
-        $scope.$apply();
-        console.log("RoomData loaded: ", data);
-      });
-    } else {
-      $scope.roomData = null;
-    }
-    _initListeners();
-  }
-  
-  function _initListeners(){
-    // CLIENT EVENTS //
+    .controller('RoomCtrl', ['$scope', 'EngineService', '$firebaseObject', '$rootScope', 'RoomService',
+        function ($scope, EngineService, $firebaseObject, $rootScope, RoomService) {
+            var ctrl = this;
+            var Rooms = firebase.database().ref('roomies/rooms');
+            ctrl.storageBasePath = "../../assets/images/furniture/";
+            function _init() {
+                ctrl.availableItems = RoomService.getItemsInventory();
+                ctrl.roomId = window.localStorage.roomId;
+                if (window.localStorage && window.localStorage.roomId) {
+                    RoomService.getRoom(window.localStorage.roomId).then(function (data) {
+                        $scope.roomData = data;
+                        $scope.$apply();
+                        console.log("RoomData loaded: ", data);
+                    });
+                } else {
+                    // $scope.roomData = null;
+                }
+                _initListeners();
+            }
 
-    // Every user interaction on the stage triggers an event which is then updating the server.
-    $rootScope.$on('STAGE_OBJECT_CHANGED', function(e, data) {
-      console.log('STAGE_OBJECT_CHANGED', data);
-      RoomService.setItem(ctrl.roomId, data.key, data);
-    });
+            function _initListeners() {
+                // CLIENT EVENTS //
 
-    $rootScope.$on('STAGE_CREATED', function(e, data){
-      // Triggered when a new user enters.
-      // Room should be created for the new user.
-      // room ID should be saved in user session (localStorage or Firebase eventually)
-      var newRoomRef = Rooms.push();
-      newRoomRef.set({items : {}});
-      // TODO replace this with two separate calls, one for adding an item, and one for adding the item to the room.
-      ctrl.roomItemsRef = firebase.database().ref('roomies/rooms/' + newRoomRef.key + '/items');
+                // Every user interaction on the stage triggers an event which is then updating the server.
+                $rootScope.$on('STAGE_OBJECT_CHANGED', function (e, data) {
+                    console.log('STAGE_OBJECT_CHANGED', data);
+                    RoomService.setItem(ctrl.roomId, data.key, data);
+                });
 
-      for (var i = 0; i < data.items.length; i++){
-        var itemRef = ctrl.roomItemsRef.push(); // add item to room
-        itemRef.set(data.items[i]);
-      }
-      ctrl.roomId = newRoomRef.key;
-      ctrl.roomRef = newRoomRef;
-      localStorage.setItem('roomId', newRoomRef.key);
-      console.log('STAGE_CREATED', ctrl.roomId);
-    });
+                $rootScope.$on('STAGE_CREATED', function (e, data) {
+                    // Triggered when a new user enters.
+                    // Room should be created for the new user.
+                    // room ID should be saved in user session (localStorage or Firebase eventually)
+                    var newRoomRef = Rooms.push();
+                    newRoomRef.set({items: {}});
+                    // TODO replace this with two separate calls, one for adding an item, and one for adding the item to the room.
+                    ctrl.roomItemsRef = firebase.database().ref('roomies/rooms/' + newRoomRef.key + '/items');
 
-    // SERVER EVENTS //
+                    for (var i = 0; i < data.items.length; i++) {
+                        var itemRef = ctrl.roomItemsRef.push(); // add item to room
+                        itemRef.set(data.items[i]);
+                    }
+                    ctrl.roomId = newRoomRef.key;
+                    ctrl.roomRef = newRoomRef;
+                    localStorage.setItem('roomId', newRoomRef.key);
+                    console.log('STAGE_CREATED', ctrl.roomId);
+                });
 
-    // Every event  on the server triggers a client event which then produces a change in the stage.
+                // SERVER EVENTS //
 
-    $rootScope.$on('EXISTING_ROOM_LOADED', function(e, data){
-      // Triggered when a new user enters.
-      // The room should be rendered with the loaded data
-      console.log('USER_ROOM_LOADED', data);
-    });
-    $rootScope.$on('NO_EXISTING_ROOM_FOUND', function(data){
-      // Triggered when no room was found in session.
-      // Room should be rendered.
-      console.log('NO_EXISTING_ROOM_FOUND', data);
-    });
-    $rootScope.$on('ROOM_OBJECT_CHANGED', function(e, data){
-      console.log('ROOM_OBJECT_CHANGED', data);
-    });
+                // Every event  on the server triggers a client event which then produces a change in the stage.
 
-    Rooms.on("value", function(snapshot) {
-      // We should listen to when a child is changed, if it has the same ID as the one we are looking at
-      // then the change should immediately appear in the room.
-      $rootScope.$broadcast('ROOM_OBJECT_CHANGED', snapshot.val());
+                $rootScope.$on('EXISTING_ROOM_LOADED', function (e, data) {
+                    // Triggered when a new user enters.
+                    // The room should be rendered with the loaded data
+                    console.log('USER_ROOM_LOADED', data);
+                });
+                $rootScope.$on('NO_EXISTING_ROOM_FOUND', function (data) {
+                    // Triggered when no room was found in session.
+                    // Room should be rendered.
+                    console.log('NO_EXISTING_ROOM_FOUND', data);
+                });
+                $rootScope.$on('ROOM_OBJECT_CHANGED', function (e, data) {
+                    console.log('ROOM_OBJECT_CHANGED', data);
+                });
 
-    });
-  }
-  
-  _init();
-  
+                Rooms.on("value", function (snapshot) {
+                    // We should listen to when a child is changed, if it has the same ID as the one we are looking at
+                    // then the change should immediately appear in the room.
+                    $rootScope.$broadcast('ROOM_OBJECT_CHANGED', snapshot.val());
 
+                });
+            }
+            
+            ctrl.scrollToClass = function(className){
+                var $target = $(className);
+                console.log("$target.offset().top: " + $target.offset().top);
+                $("body").animate({scrollTop: $target.offset().top}, 600); // scroll to discussion top
+            };
+            
+            _init();
+            
 
-}]);
+        }]);
